0aaab7fd24fdf244b3684dbc10591ce038108ed1
bnjix <bnjix@hotmail.com>
2018-09-17 15:54:36 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='shapely', # This is the name of your PyPI-package.
+    version='1.6b4', # Update the version number for new releases
+    packages=find_packages(),
+    include_package_data=True
+)
